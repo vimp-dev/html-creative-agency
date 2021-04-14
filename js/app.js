07b03481085db7c937d816fe51547bec23b944b7
Timur <navsta01@gmail.com>
2021-04-14 13:15:46 +0400

    add final sections

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 const toggle = document.querySelector('.js-toggle-menu');
 const menu = document.querySelector(`${toggle.dataset.menu}`);
+const mainNav = document.querySelector('#main-menu');
+
+mainNav.addEventListener('click', (e) => {
+    if(e.target.tagName === 'A') {
+        menu.classList.remove('opened');
+        toggle.target.classList.remove('opened');
+    }
+});
 
 toggle.addEventListener('click', (e) => {
     menu.classList.toggle('opened');
@@ -14,4 +22,16 @@ const slider = new Splide('.splide', {
 
 slider.on('moved', () => {
     slider.root.querySelector('.splide__list').classList.toggle('toggle-slides');
+});
+
+document.querySelectorAll('.form__input').forEach(item => {
+    item.addEventListener('focus', () => {
+        item.nextElementSibling.classList.add('active');
+    });
+    
+    item.addEventListener('blur', () => {
+        if(item.value.trim() === '') {
+            item.nextElementSibling.classList.remove('active');
+        }
+    });
 });
